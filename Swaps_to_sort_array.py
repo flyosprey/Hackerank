edfd71b6bc3c3889b4991a32b42f6ca2c0dcd182
flyosprey <70664311+flyosprey@users.noreply.github.com>
2021-04-17 19:41:37 +0300

    Add description for Swaps

--- Swaps_to_sort_array.py
+++ Swaps_to_sort_array.py
@@ -1,6 +1,14 @@
 from time import time
 
 
+""" Problem:
+    You are given an unordered array consisting of consecutive integers
+    [1, 2, 3, ..., n] without any duplicates.
+    You are allowed to swap any two elements.
+    Find the minimum number of swaps required to sort
+    the array in ascending order.
+"""
+
 def minimumSwap(arr):
     start = time()
     ref_arr = sorted(arr)
